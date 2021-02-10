6b55a78560ee2e8cce881d6da15622269ba5a2e3
Myles <mylesblasonato988@hotmail.com>
2021-02-10 20:28:29 +1100

    first pass

---
+++ space_invaders_bolt_2d/Assets/Pipeline/MyRenderPipelineInstance.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MyRenderPipelineInstance : RenderPipeline
+{
+    // Use this variable to a reference to the Render Pipeline Asset that was passed to the constructor
+    private MyRenderPipelineAsset renderPipelineAsset;
+
+    // The constructor has an instance of the ExampleRenderPipelineAsset class as its parameter.
+    public MyRenderPipelineInstance(MyRenderPipelineAsset asset)
+    {
+        renderPipelineAsset = asset;
+    }
+
+    // Unity calls this method once per frame for each CameraType that is currently rendering.
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+        // This is an example of using the data from the Render Pipeline Asset.
+        Debug.Log(renderPipelineAsset.exampleString);
+
+        // This is where you can write custom rendering code. Customize this method to customize your SRP.
+    }
+}
