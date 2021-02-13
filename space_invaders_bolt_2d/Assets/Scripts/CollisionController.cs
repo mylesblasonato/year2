cd5f92a6492996204e3156fd68ae086344236f6b
Myles <mylesblasonato988@hotmail.com>
2021-02-14 10:44:44 +1100

    added enemies and lives

---
+++ space_invaders_bolt_2d/Assets/Scripts/CollisionController.cs
@@ -0,0 +1,26 @@
+﻿using Bolt;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionController : MonoBehaviour
+{
+    [SerializeField] FlowMachine _flowMachine;
+    [SerializeField] string _collidedObjectName = "LeftWall";
+    [SerializeField] string _boltEventName = "GoRight";
+    [SerializeField] string _tag = "Bullet";
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (string.Compare(collider.name, _collidedObjectName) == 0)
+        {
+            _flowMachine.TriggerUnityEvent(_boltEventName);
+        }
+
+        if (_tag!="" && collider.CompareTag(_tag))
+        {
+            _flowMachine.TriggerUnityEvent(_boltEventName);
+        }
+    }
+}
